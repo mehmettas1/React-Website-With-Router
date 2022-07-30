cfa773dab635f14cdb3093e88b793468e53a04eb
mehmet <tas349873@gmail.com>
2022-07-30 21:03:49 +0300

    fınıshed

--- src/pages/Css.jsx
+++ src/pages/Css.jsx
@@ -1,5 +1,16 @@
+import Card from "../components/Card";
+import { useLocation } from "react-router-dom";
+
+
 const Css = () => {
-  return <div>Css</div>;
+  const location = useLocation();
+  const cssInfo = location.state;
+  console.log(cssInfo);
+  return (
+    <div className="pages-height py-5">
+      <Card {...cssInfo} />
+    </div>
+  )
 };
 
 export default Css;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import htmllogo from "../assets/img/logo_html.png"
-import csslogo from "../assets/img/logo_brush.png"
-import brushlogo from "../assets/img/logo_css.png"
-
+import brushlogo from "../assets/img/logo_brush.png"
+import csslogo from "../assets/img/logo_css.png"
+import { useNavigate } from "react-router-dom";
+import {info} from "../assets/data";
+import Subscribe from "../components/Subscribe";
 
 const Home = () => {
+  const navigate = useNavigate();
+  console.log(info);
   return(
     <div>
       <main>
@@ -15,38 +19,21 @@ const Home = () => {
       condimentum.
     </p>
   </section>
-  <section className="subscribe">
-    <div id="newsletter">
-      <p>Subscribe To Our Newsletter</p>
-    </div>
-    <div>
-      <form action="#">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter Email..."
-        />
-        <button type="submit" id="submit">
-          Subscribe
-        </button>
-      </form>
-    </div>
-  </section>
+  <Subscribe/>
   <section class="logo">
-            <div class="box">
+            <div class="box" onClick={()=>navigate("/html",{state:info[0].htmlInfo})} >
                 <img src={htmllogo} alt="html_logo"/>
                 <h3>HTML5 Markup</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
                 </p>
             </div>
-            <div class="box">
+            <div class="box"onClick={()=>navigate("/css",{state:info[1].cssInfo})}>
                 <img src={csslogo} alt="css_logo"/>
                 <h3>CSS3 Styling</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
                 </p>
             </div>
-            <div class="box">
+            <div class="box"onClick={()=>navigate("/logo",{state:info[2].logoInfo})}>
                 <img src= {brushlogo} alt="graphic_logo"/>
                 <h3>Graphic Design</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
