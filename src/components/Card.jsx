ccec126e725ab30306f83bbc6889723e7180c04e
mehmet <tas349873@gmail.com>
2022-07-30 19:07:51 +0300

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,4 @@
-import htmllogo from "../assets/img/logo_html.png"
-import csslogo from "../assets/img/logo_brush.png"
-import brushlogo from "../assets/img/logo_css.png"
+
 
 
 
@@ -8,26 +6,7 @@ import brushlogo from "../assets/img/logo_css.png"
 const Card = () => {
   return (
   <div>
-      <section class="logo">
-            <div class="box">
-                <img src={htmllogo} alt="html_logo"/>
-                <h3>HTML5 Markup</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
-                </p>
-            </div>
-            <div class="box">
-                <img src={csslogo} alt="css_logo"/>
-                <h3>CSS3 Styling</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
-                </p>
-            </div>
-            <div class="box">
-                <img src= {brushlogo} alt="graphic_logo"/>
-                <h3>Graphic Design</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
-                </p>
-            </div>
-        </section>
+      
   </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
+import htmllogo from "../assets/img/logo_html.png"
+import csslogo from "../assets/img/logo_brush.png"
+import brushlogo from "../assets/img/logo_css.png"
+
+
 const Home = () => {
   return(
     <div>
@@ -28,6 +33,26 @@ const Home = () => {
       </form>
     </div>
   </section>
+  <section class="logo">
+            <div class="box">
+                <img src={htmllogo} alt="html_logo"/>
+                <h3>HTML5 Markup</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
+                </p>
+            </div>
+            <div class="box">
+                <img src={csslogo} alt="css_logo"/>
+                <h3>CSS3 Styling</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
+                </p>
+            </div>
+            <div class="box">
+                <img src= {brushlogo} alt="graphic_logo"/>
+                <h3>Graphic Design</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
+                </p>
+            </div>
+        </section>
       </main>
     </div>
   );
